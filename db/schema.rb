4ee707c9049fdbb0e3507e7340f295a9c13dcb9a
Varun Wachaspati J <varunwachaspati@gmail.com>
2014-11-05 03:04:03 +0530

    Postgresql installed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140326171554) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entrepreneurs", force: true do |t|
     t.string   "fname"
     t.string   "lname"
@@ -24,8 +27,8 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.string   "remember_token"
   end
 
-  add_index "entrepreneurs", ["email"], name: "index_entrepreneurs_on_email", unique: true
-  add_index "entrepreneurs", ["remember_token"], name: "index_entrepreneurs_on_remember_token"
+  add_index "entrepreneurs", ["email"], name: "index_entrepreneurs_on_email", unique: true, using: :btree
+  add_index "entrepreneurs", ["remember_token"], name: "index_entrepreneurs_on_remember_token", using: :btree
 
   create_table "follows", force: true do |t|
     t.string   "follower_type"
@@ -35,8 +38,8 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.datetime "created_at"
   end
 
-  add_index "follows", ["followable_id", "followable_type"], name: "fk_followables"
-  add_index "follows", ["follower_id", "follower_type"], name: "fk_follows"
+  add_index "follows", ["followable_id", "followable_type"], name: "fk_followables", using: :btree
+  add_index "follows", ["follower_id", "follower_type"], name: "fk_follows", using: :btree
 
   create_table "likes", force: true do |t|
     t.string   "liker_type"
@@ -46,8 +49,8 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.datetime "created_at"
   end
 
-  add_index "likes", ["likeable_id", "likeable_type"], name: "fk_likeables"
-  add_index "likes", ["liker_id", "liker_type"], name: "fk_likes"
+  add_index "likes", ["likeable_id", "likeable_type"], name: "fk_likeables", using: :btree
+  add_index "likes", ["liker_id", "liker_type"], name: "fk_likes", using: :btree
 
   create_table "mentions", force: true do |t|
     t.string   "mentioner_type"
@@ -57,8 +60,8 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.datetime "created_at"
   end
 
-  add_index "mentions", ["mentionable_id", "mentionable_type"], name: "fk_mentionables"
-  add_index "mentions", ["mentioner_id", "mentioner_type"], name: "fk_mentions"
+  add_index "mentions", ["mentionable_id", "mentionable_type"], name: "fk_mentionables", using: :btree
+  add_index "mentions", ["mentioner_id", "mentioner_type"], name: "fk_mentions", using: :btree
 
   create_table "microposts", force: true do |t|
     t.text     "content"
@@ -67,7 +70,7 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.datetime "updated_at"
   end
 
-  add_index "microposts", ["startup_id", "created_at"], name: "index_microposts_on_startup_id_and_created_at"
+  add_index "microposts", ["startup_id", "created_at"], name: "index_microposts_on_startup_id_and_created_at", using: :btree
 
   create_table "startups", force: true do |t|
     t.string   "name"
@@ -89,7 +92,7 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.string   "tagline"
   end
 
-  add_index "startups", ["entrepreneur_id", "created_at"], name: "index_startups_on_entrepreneur_id_and_created_at"
+  add_index "startups", ["entrepreneur_id", "created_at"], name: "index_startups_on_entrepreneur_id_and_created_at", using: :btree
 
   create_table "students", force: true do |t|
     t.string   "fname"
@@ -114,8 +117,8 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.string   "phone"
   end
 
-  add_index "students", ["email"], name: "index_students_on_email", unique: true
-  add_index "students", ["remember_token"], name: "index_students_on_remember_token"
+  add_index "students", ["email"], name: "index_students_on_email", unique: true, using: :btree
+  add_index "students", ["remember_token"], name: "index_students_on_remember_token", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "linkedin_id"
@@ -125,6 +128,6 @@ ActiveRecord::Schema.define(version: 20140326171554) do
     t.string   "email"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
